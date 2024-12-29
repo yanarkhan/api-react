5b60ffe748692ad142809be447bfba2d66ce19d9
yanarkhan <rianarkhan@gmail.com>
2024-12-29 19:10:01 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <h1 className="font-bold text-xl underline">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
